584a1c559fd5686aa2048ecbcb53cc7ec281de9f
matthew hallberg <matthewhallberg@matthews-MBP.fios-router.home>
2020-12-06 00:26:15 -0500

    first commit

---
+++ Assets/Scripts/CamFeed.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class CamFeed : MonoBehaviour {
+
+    WebCamTexture webcamTexture;
+    bool videoStarted;
+
+    void Start() {
+        StartWebCam();
+    }
+
+    void Update() {
+        if (!videoStarted && webcamTexture.width > 100) {
+            SetAspectRatio();
+        }
+    }
+
+    void StartWebCam() {
+        //logitech 1920 by 1080
+        webcamTexture = new WebCamTexture(GetWebCamDevice(),960,540,30);
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+
+    void SetAspectRatio() {
+        Vector3 tempScale = transform.localScale;
+        float aspectRatio = (float)webcamTexture.width / (float)webcamTexture.height;
+        tempScale.x = aspectRatio;
+        transform.localScale = tempScale;
+        videoStarted = true;
+    }
+
+    string GetWebCamDevice() {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        foreach (WebCamDevice device in devices) {
+            print(device.name);
+        }
+        return devices[1].name;
+    }
+}
